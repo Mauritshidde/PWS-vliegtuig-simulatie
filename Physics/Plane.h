32185a39ad1ef2fbc118ab2989f977a618c62233
Mauritshidde <Mauritshidde@gmail.com>
2023-10-04 11:42:31 +0200

    plane class
    functions naming of the plane class

--- Physics/Plane.h
+++ Physics/Plane.h
@@ -5,18 +5,35 @@ class Plane {
 private:
       /* data */
 public:
-      Vector3 pos;
+      Plane(float givenMass, Vector3 startingPos);
+      ~Plane();
+      Vector3 calcLift();
+      Vector3 calcCenterOfLiftWing(Vector3 startOfWing, Vector3 endOfWing, float startWingWidth, float endWingWidth);
+      
+      Vector3 pos, centerOfMass, centerOfLiftWingR, centerOfLiftWingL;
       float speed;
       float anglePitch, angleYaw, angleRoll;
       float mass;
-      
-      Plane();
-      ~Plane();
 };
 
-Plane::Plane() {
+Plane::Plane(float givenMass, Vector3 startingPos) {
+      mass = givenMass;
       speed = 0;
       anglePitch = 0;
       angleYaw = 0;
       angleRoll = 0;
+      // centerOfLiftWingL = calcCenterOfLiftWing();
+      // centerOfLiftWingR = calcCenterOfLiftWing();
+}
+
+Plane::~Plane() {
+
+}
+
+Vector3 Plane::calcLift() {
+      // TODO lift formula
+}
+
+Vector3 Plane::calcCenterOfLiftWing(Vector3 startOfWing, Vector3 endOfWing, float startWingWidth, float endWingWidth) {
+      // TODO lift formula
 }
