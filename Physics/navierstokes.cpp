1d705d13f3e7804a7954708cbea1d067500c7425
Mauritshidde <Mauritshidde@gmail.com>
2023-12-06 09:50:30 +0100

    code voor berekenen u en v
    nieuwe u en v over de tijd bereklenen en betere code voor het maken van de mesh

--- Physics/navierstokes.cpp
+++ Physics/navierstokes.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <raylib.h>
+#include <math.h>
 
 class NavierStokes
 {
@@ -41,7 +42,7 @@ NavierStokes::NavierStokes()
       ny = 50;
       iMin = 0;
       iMax = nx - 1;
-      jMin = 0;
+      jMin = 1; // jMin has to be 1;
       jMax = ny - 1;
       Lx = 100;
       Ly = 100;
@@ -79,39 +80,56 @@ void NavierStokes::createMesh()
       dxi = 1 / dx;
       dyi = 1 / dy;
 
-      // for (int i = iMin-1; i < iMax+2; i++)
-      // {
-      //       std::vector<float> helper;
-      //       for (int j=jMin-1; j < jMax+2; j++) {
-      //             helper.push_back(0);
-      //       }
-      //       u.push_back(helper);
-      // }
-      
-
-      for (int i=0; i < xm.size(); i++) {
+      for (int i = iMin-1; i < iMax+2; i++)
+      {
             std::vector<float> helper;
-            for (int j=0; j < ym.size(); j++) {
+            for (int j=jMin-1; j < jMax+2; j++) {
                   helper.push_back(0);
             }
-            p.push_back(helper);
+            u.push_back(helper);
       }
 
-      for (int i=0; i < x.size(); i++) {
+      for (int i = iMin-1; i < iMax+2; i++)
+      {
             std::vector<float> helper;
-            for (int j=0; j < y.size(); j++) {
+            for (int j=jMin-1; j < jMax+2; j++) {
                   helper.push_back(0);
             }
             v.push_back(helper);
       }
 
-      for (int i=0; i < x.size(); i++) {
+      for (int i = iMin-1; i < iMax+2; i++)
+      {
             std::vector<float> helper;
-            for (int j=0; j < y.size(); j++) {
+            for (int j=jMin-1; j < jMax+2; j++) {
                   helper.push_back(0);
             }
-            u.push_back(helper);
+            p.push_back(helper);
       }
+
+      // for (int i=0; i < xm.size(); i++) {
+      //       std::vector<float> helper;
+      //       for (int j=0; j < ym.size(); j++) {
+      //             helper.push_back(0);
+      //       }
+      //       p.push_back(helper);
+      // }
+
+      // for (int i=0; i < x.size(); i++) {
+      //       std::vector<float> helper;
+      //       for (int j=0; j < y.size(); j++) {
+      //             helper.push_back(0);
+      //       }
+      //       v.push_back(helper);
+      // }
+
+      // for (int i=0; i < x.size(); i++) {
+      //       std::vector<float> helper;
+      //       for (int j=0; j < y.size(); j++) {
+      //             helper.push_back(0);
+      //       }
+      //       u.push_back(helper);
+      // }
 
       for (int i=iMin; i < iMax+1; i++) {
             boundaryMinU.push_back(0);
@@ -127,13 +145,17 @@ void NavierStokes::createMesh()
 
 void NavierStokes::boundaryConditions() {
       for (int i=iMin; i < iMax+1; i++) {
-            boundaryMinU.at(i) = u.at(i).at(jMin) - 2 * (u.at(i).at(jMin) - uBottom);
-            boundaryMaxU.at(i) = u.at(i).at(jMax) - 2 * (u.at(i).at(jMax) - uTop);
+            u.at(i).at(jMin-1) = u.at(i).at(jMin) - 2 * (u.at(i).at(jMin) - uBottom);
+            u.at(i).at(jMax+1) = u.at(i).at(jMax) - 2 * (u.at(i).at(jMax) - uBottom);
+            // boundaryMinU.at(i) = u.at(i).at(jMin) - 2 * (u.at(i).at(jMin) - uBottom);
+            // boundaryMaxU.at(i) = u.at(i).at(jMax) - 2 * (u.at(i).at(jMax) - uTop);
       }
 
-      for (int i=jMin; i < jMax+1; i++) {
-            boundaryMinV.at(i) = u.at(iMin).at(i) - 2 * (u.at(iMin).at(i) - uBottom);
-            boundaryMaxV.at(i) = u.at(iMax).at(i) - 2 * (u.at(iMax).at(i) - uTop);
+      for (int j=jMin; j < jMax+1; j++) {
+            u.at(iMin-1).at(j) = u.at(iMin).at(j) - 2 * (u.at(iMin).at(j) - uBottom);
+            u.at(iMax+1).at(j) = u.at(iMax).at(j) - 2 * (u.at(iMax).at(j) - uBottom);
+            // boundaryMinV.at(i) = u.at(iMin).at(i) - 2 * (u.at(iMin).at(i) - uBottom);
+            // boundaryMaxV.at(i) = u.at(iMax).at(i) - 2 * (u.at(iMax).at(i) - uTop);
       }
 }
 
@@ -145,6 +167,37 @@ void NavierStokes::calc() {
       while(time < maxTime) {
             time += dT;
             boundaryConditions();
+
+            // for (int i=jMin; i < jMax; i++) {
+            //       for (int j=iMin+1; j < iMax; j++) {
+
+            //       }
+            // }
+
+            for (int j=jMin; j < jMax+1; j++) {
+                  for (int i=iMin+1; i < iMax+1; j++) {
+                        float vHere = 0.25 * (v.at(i-1).at(j) + v.at(i-1).at(j+1) + v.at(i).at(j) + v.at(i).at(j+1));
+                        float a = (nu * (u.at(i-1).at(j) -2 * u.at(i).at(j) + u.at(i+1).at(j)) * pow(dxi, 2));
+                        float b = nu * (u.at(i).at(j-1) -2 * u.at(i).at(j) + u.at(i).at(j+1) * pow(dyi, 2));
+                        float c = -u.at(i).at(j) * (u.at(i+1).at(j) - u.at(i-1).at(j)) * 0.5 * dxi;
+                        float d = -vHere * (u.at(i).at(j+1) - u.at(i).at(j-1)) * 0.5 * dyi;
+                        float us = u.at(i).at(j) + dT * (a + b + c + d);
+
+                  }
+            }
+
+            for (int j=jMin+1; j < jMax+1; j++) {
+                  for (int i=iMin; i < iMax+1; i++) {
+                        float uHere = 0.25 * (u.at(i).at(j-1) + u.at(i).at(j) + u.at(i+1).at(j-1) + u.at(i+1).at(j));
+                        float a = (nu * (v.at(i-1).at(j) -2 * v.at(i).at(j) + v.at(i+1).at(j)) * pow(dxi, 2));
+                        float b = nu * (v.at(i).at(j-1) -2 * v.at(i).at(j) + v.at(i).at(j+1) * pow(dyi, 2));
+                        float c = -uHere * (v.at(i+1).at(j) - v.at(i-1).at(j)) * 0.5 * dyi;
+                        float d = -v.at(i).at(j) * (v.at(1).at(j+1) - v.at(i).at(j-1)) * 0.5 * dxi;
+                        float vs = v.at(i).at(j) + dT * (a + b + c + d);
+                  }
+            }
+
+            // float changeInU = 
       }
 }
 
