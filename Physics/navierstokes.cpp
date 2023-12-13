08594459f2dc2486d6041f866fed5617463c6e2d
kaioraf <45394248+kaioraf@users.noreply.github.com>
2023-12-13 16:29:46 +0100

    Matplot lib add and plotting

--- Physics/navierstokes.cpp
+++ Physics/navierstokes.cpp
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <bits/stdc++.h>
 
-#include <matplot/matplot.h>
+#include "matplotlibcpp.h"
+namespace mat = matplotlibcpp;
 class NavierStokes
 {
 private:
@@ -42,6 +43,7 @@ public:
       void createMesh();
       void boundaryConditions();
       std::vector<std::vector<float>> calcL(std::vector<std::vector<float>> L);
+      void plot();
       void calc();
 };
 
@@ -69,9 +71,8 @@ NavierStokes::NavierStokes()
       detOfL = determinantOfMatrix(L, L.size());
 
       invL = inverse(L, invL);
-}
+} 
 
-#include <vector>
 
 void getCofactor(std::vector<std::vector<float>> A, std::vector<std::vector<float>> temp, int p, int q, int n)
 {
@@ -193,6 +194,13 @@ std::vector<std::vector<float>> NavierStokes::calcL(std::vector<std::vector<floa
             }
             L.at(0).at(0) = 1;
       }
+}
+
+void NavierStokes::plot()
+{
+
+      // mat::quiver(x, y, u, v);
+      // mat::show();
 }
 
 std::vector<float> linspace(int startX, int endX, int steps)
--- Physics/plot.cpp
+++ Physics/plot.cpp
@@ -1,19 +1,45 @@
-#include <cmath>
-#include <matplot/matplot.h>
-
+#include "matplotlibcpp.h"
+namespace mat = matplotlibcpp;
 int main()
 {
-      double x, y = 0;
-      auto [x, y] = matplot::meshgrid(matplot::iota(0.0, 0.2, 2.0), matplot::iota(0.0, 0.2, 2.0));
-      matplot::vector_2d u =
-          matplot::transform(x, y, [](double x, double y)
-                             { return cos(x) * y; });
-      matplot::vector_2d v =
-          matplot::transform(x, y, [](double x, double y)
-                             { return sin(x) * y; });
+    // u and v are respectively the x and y components of the arrows we're plotting
+    std::vector<int> x, y, u, v;
+    for (int i = -5; i <= 5; i++) {
+        for (int j = -5; j <= 5; j++) {
+            x.push_back(i);
+            u.push_back(-i);
+            y.push_back(j);
+            v.push_back(-j);
+        }
+    }
+    // std::cout << "{";
+    // for (int i = 0; i < x.size(); i++)
+    // {
+    //     std::cout << x.at(i) << ", ";
+    // }
+    // std::cout << "} \n";
 
-      matplot::quiver(x, y, u, v);
+    // std::cout << "{";
+    // for (int i = 0; i < y.size(); i++)
+    // {
+    //     std::cout << y.at(i) << ", ";
+    // }
+    // std::cout << "} \n";
 
-      matplot::show();
-      return 0;
+    std::cout << "{";
+    for (int i = 0; i < u.size(); i++)
+    {
+        std::cout << u.at(i) << ", ";
+    }
+    std::cout << "} \n";
+
+    std::cout << "{";
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v.at(i) << ", ";
+    }
+    std::cout << "} \n";
+
+    mat::quiver(x, y, u, v);
+    mat::show();
 }
