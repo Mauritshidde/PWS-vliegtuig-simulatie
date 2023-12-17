e354de942c674a156267a61cc304eceffd61670e
Mauritshidde <Mauritshidde@gmail.com>
2023-12-18 00:51:50 +0100

    oude code terug gehaald
    code die ik had verwijderd maar eigenlijk wel nog nodig was in matrix.h terug geplaatst

--- Physics/matrix.h
+++ Physics/matrix.h
@@ -13,8 +13,8 @@
 // std::vector<std::vector<float>> getCofactor(const std::vector<std::vector<float>> vect);
 // std::vector<std::vector<float>> getInverse(const std::vector<std::vector<float>> vect);
 
-// std::vector<std::vector<float>> zeros(int width, int height);
 // float det(std::vector<std::vector<float>> A);
-// float determinantOfMatrix(std::vector<std::vector<float>> mat, int n);
-// std::vector<float> linspace(int startX, int endX, int steps);
+std::vector<std::vector<float>> zeros(int width, int height);
+float determinantOfMatrix(std::vector<std::vector<float>> mat, int n);
+std::vector<float> linspace(int startX, int endX, int steps);
 std::vector<std::vector<float>> invertMatrix(std::vector<std::vector<float>> matrix, int size);
