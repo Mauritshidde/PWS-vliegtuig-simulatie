d78fd380c7029287a3624f25d8c286a1f77bc7b0
Mauritshidde <Mauritshidde@gmail.com>
2024-01-01 20:26:48 +0100

    begin van code voor maken van lift file
    json.cpp is het begin van de code voor het crearen van lift constant file

---
+++ json.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <iostream>
+
+using json = nlohmann::json;
+
+
+void createLiftFile(int steps, float precisionFactor) { // steps has to be greater than 10 but realisticly has to be higher than 200
+    json data;
+
+    float pitchAngle = 0;
+    float yawAngle = 0;
+    float stepSize = 360.0f/(steps-1);
+
+    for (int i=0; i < steps; i++) {
+        float roundedPitchAngle = (int)(pitchAngle / precisionFactor) * precisionFactor;
+        std::cout << roundedPitchAngle << " ";
+        for (int j=0; j < steps; j++) {
+            float roundedYawAngle = (int)(yawAngle / precisionFactor) * precisionFactor;
+            data["pitch"][std::to_string(roundedPitchAngle)]["yaw"][std::to_string(roundedYawAngle)]["cl"] = 3;
+            data["pitch"][std::to_string(roundedPitchAngle)]["yaw"][std::to_string(roundedYawAngle)]["cd"] = 5;
+            yawAngle += stepSize;
+        }
+        pitchAngle += stepSize;
+        yawAngle = 0;
+        // pitchAngle += stepsize;
+        // for (int j=0; yawAngle < 360; j++) {
+
+        // }
+        // pitchAngle += 
+    }
+    std::cout << std::endl;
+
+    data["precisionFactor"] = precisionFactor; // amount of decimal places for pitch angle and yaw angle
+
+    std::ofstream liftfile;
+    liftfile.open ("test.json");
+    liftfile << data;
+    liftfile.close();
+}
+
+int main() {
+    // std::ifstream f("2.json");
+    // json data = json::parse(f);
+    json data;
+    // json ex3 = {
+    //     {"happy", true},
+    //     {"pi", 3.141},
+    // };
+
+    float pitchAngle = 0;
+    float yawAngle = 0;
+    int steps = 100;
+
+    float stepSize = 360.0f/(steps-1);
+    // float x = 360.0f/(steps-1);
+    float precisionFactor = 0.01;  // i.e. round to nearest one-hundreth
+    createLiftFile(steps, precisionFactor);
+    
+    // float value = (int)(stepSize / precisionFactor) * precisionFactor;
+    // std::cout << value << " " << x<< std::endl;
+
+    // // auto j3 = json::parse(R"({"happy": true, "pi": 3.141})");
+    // for (int i=0; i < steps; i++) {
+    //     std::cout << stepSize * i << " ";
+    //     for (int j=0; j < steps; j++) {
+    //         data["pitch"][std::to_string(pitchAngle)]["yaw"][std::to_string(yawAngle)]["cl"] = 3;
+    //         data["pitch"][std::to_string(pitchAngle)]["yaw"][std::to_string(yawAngle)]["cd"] = 5;
+    //         yawAngle += stepSize;
+    //     }
+    //     pitchAngle += stepSize;
+    //     yawAngle = 0;
+    // }
+    // std::cout << std::endl;
+    // data["precisionFactor"] = precisionFactor; // amount of decimal places for pitch angle and yaw angle
+    // // data["pitch"][std::to_string(1)]["yaw"][std::to_string(5)]["cl"] = 3;
+    // // data["pitch"][std::to_string(1)]["yaw"][std::to_string(5)]["cd"] = 5;
+
+
+    // std::ofstream testfile;
+    // testfile.open ("test.json");
+    // testfile << data;
+    // testfile.close();
+
+
+    return 0;
+}
