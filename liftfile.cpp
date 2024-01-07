a8d7a87772d529d754fb904133e3966599eb8c0b
Mauritshidde <Mauritshidde@gmail.com>
2024-01-07 21:26:52 +0100

    code voor uitlezen json file af
    maken van json file code moet nog worden aangevuld

--- liftfile.cpp
+++ liftfile.cpp
@@ -20,42 +20,121 @@ void createLiftFile(int steps) { // steps has to be greater than 2 but realistic
     liftfile.close();
 }
 
-Vector2 getConstFromLiftFile(float pitchAngle, float yawAngle) {
+Vector2 getClCdWithYaw(float yawAngle, nlohmann::json constFile) {
     float cl, cd;
-    
-    std::ifstream f("test.json");
-    nlohmann::json data = nlohmann::json::parse(f);
-    f.close();
 
-    float stepSize = data["stepSize"].get<float>();
+    float stepSize = constFile["stepSize"].get<float>();
+
+    float translatedYaw = yawAngle/stepSize;
+    int yawIndex = (int) translatedYaw;
+
+    float Cl1 = constFile["yaw"][std::to_string(yawIndex)]["cl"].get<float>() * (1 - (translatedYaw - yawIndex));
+    float Cl2 = constFile["yaw"][std::to_string(yawIndex + 1)]["cl"].get<float>() * (translatedYaw - yawIndex);
+
+    cl = Cl1 + Cl2;
+
+    float Cd1 = constFile["yaw"][std::to_string(yawIndex)]["cd"].get<float>() * (1 - (translatedYaw - yawIndex));
+    float Cd2 = constFile["yaw"][std::to_string(yawIndex + 1)]["cd"].get<float>() * (translatedYaw - yawIndex);
+
+    cd = Cd1 + Cd2;
+    
+    std::cout << translatedYaw << " " << cl << std::endl;
+
+    return {cl, cd};
+}
+
+Vector2 getClCdWithPitch(float pitchAngle, nlohmann::json constFile) {
+    float cl, cd;
+
+    float stepSize = constFile["stepSize"].get<float>();
+
     float translatedPitch = pitchAngle/stepSize;
     int pitchIndex = (int) translatedPitch;
 
-    float translatedYaw = yawAngle/stepSize;
-    int yawIndex = (int) translatedPitch;
+    float Cl1 = constFile["pitch"][std::to_string(pitchIndex)]["cl"].get<float>() * (1 - (translatedPitch - pitchIndex));
+    float Cl2 = constFile["pitch"][std::to_string(pitchIndex + 1)]["cl"].get<float>() * (translatedPitch - pitchIndex);
 
-    float Cl1 = data["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex)]["cl"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (1 - (translatedYaw - yawIndex));
-    float Cl2 = data["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex)]["cl"].get<float>() * (translatedPitch - pitchIndex) * (1 - (translatedYaw - yawIndex));
+    cl = Cl1 + Cl2;
 
-    float Cl3 = data["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex + 1)]["cl"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (translatedYaw - yawIndex);
-    float Cl4 = data["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex + 1)]["cl"].get<float>() * (translatedPitch - pitchIndex) * (translatedYaw - yawIndex);
+    float Cd1 = constFile["pitch"][std::to_string(pitchIndex)]["cd"].get<float>() * (1 - (translatedPitch - pitchIndex));
+    float Cd2 = constFile["pitch"][std::to_string(pitchIndex + 1)]["cd"].get<float>() * (translatedPitch - pitchIndex);
 
-    cl = Cl1 + Cl2 + Cl3 + Cl4;
-
-    float Cd1 = data["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex)]["cd"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (1 - (translatedYaw - yawIndex));
-    float Cd2 = data["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex)]["cd"].get<float>() * (translatedPitch - pitchIndex) * (1 - (translatedYaw - yawIndex));
-
-    float Cd3 = data["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex + 1)]["cd"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (translatedYaw - yawIndex);
-    float Cd4 = data["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex + 1)]["cd"].get<float>() * (translatedPitch - pitchIndex) * (translatedYaw - yawIndex);
-
-    cd = Cd1 + Cd2 + Cd3 + Cd4;
-
+    cd = Cd1 + Cd2;
+    
     std::cout << translatedPitch << " " << cl << std::endl;
 
     return {cl, cd};
 }
 
+Vector2 getClCdWithPitchAndYaw(float pitchAngle, float yawAngle, nlohmann::json constFile) {
+    float cl, cd;
+
+    float stepSize = constFile["stepSize"].get<float>();
+    float translatedPitch = pitchAngle/stepSize;
+    int pitchIndex = (int) translatedPitch;
+
+    float translatedYaw = yawAngle/stepSize;
+    int yawIndex = (int) translatedYaw;
+
+    float Cl1 = constFile["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex)]["cl"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (1 - (translatedYaw - yawIndex));
+    float Cl2 = constFile["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex)]["cl"].get<float>() * (translatedPitch - pitchIndex) * (1 - (translatedYaw - yawIndex));
+
+    float Cl3 = constFile["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex + 1)]["cl"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (translatedYaw - yawIndex);
+    float Cl4 = constFile["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex + 1)]["cl"].get<float>() * (translatedPitch - pitchIndex) * (translatedYaw - yawIndex);
+
+    cl = Cl1 + Cl2 + Cl3 + Cl4;
+
+    float Cd1 = constFile["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex)]["cd"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (1 - (translatedYaw - yawIndex));
+    float Cd2 = constFile["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex)]["cd"].get<float>() * (translatedPitch - pitchIndex) * (1 - (translatedYaw - yawIndex));
+
+    float Cd3 = constFile["pitch"][std::to_string(pitchIndex)]["yaw"][std::to_string(yawIndex + 1)]["cd"].get<float>() * (1 - (translatedPitch - pitchIndex)) * (translatedYaw - yawIndex);
+    float Cd4 = constFile["pitch"][std::to_string(pitchIndex + 1)]["yaw"][std::to_string(yawIndex + 1)]["cd"].get<float>() * (translatedPitch - pitchIndex) * (translatedYaw - yawIndex);
+
+    cd = Cd1 + Cd2 + Cd3 + Cd4;
+    
+    std::cout << translatedPitch << " " << cl << std::endl;
+
+    return {cl, cd};
+}
+
+Vector2 getConstFromLiftFile(float pitchAngle, float yawAngle, bool withYaw, bool withPitch, nlohmann::json constFile) {
+    float cl, cd;
+    
+    if (withYaw || withPitch) {
+        std::ifstream f("planes/liftfiles/Boeing737.json");
+        nlohmann::json constFile = nlohmann::json::parse(f);
+        f.close();
+        Vector2 vals = getClCdWithPitchAndYaw(pitchAngle, yawAngle, constFile);
+        cl = vals.x;
+        cd = vals.y;
+
+        return {cl, cd};
+    } else if (withYaw) {
+        std::ifstream f("planes/liftfiles/Boeing737Yaw.json");
+        nlohmann::json constFile = nlohmann::json::parse(f);
+        f.close();
+        Vector2 vals = getClCdWithYaw(yawAngle, constFile);
+        cl = vals.x;
+        cd = vals.y;
+
+        return {cl, cd};
+    } else {
+        std::ifstream f("planes/liftfiles/Boeing737Pitch.json");
+        nlohmann::json constFile = nlohmann::json::parse(f);
+        f.close();
+        Vector2 vals = getClCdWithPitch(pitchAngle, constFile);
+        cl = vals.x;
+        cd = vals.y;
+
+        return {cl, cd};
+    }
+
+}
+
 // int main() {
+        // std::ifstream f("test.json");
+        // nlohmann::json constFile = nlohmann::json::parse(f);
+        // f.close();
 //     float pitchAngle = 0;
 //     float yawAngle = 0;
 //     int steps = 100;
--- liftfile.h
+++ liftfile.h
@@ -7,4 +7,7 @@
 void createLiftFile(int steps); // steps has to be greater than 2 but realisticly has to be higher than 200 and lower than 1000 for perfomance reasons
 void fileWithoutYaw(float precisionFactor, bool simpleStepType);
 void fileWithoutPitch(float precisionFactor, bool simpleStepType);
-Vector2 getConstFromLiftFile(float pitchAngle, float yawAngle);
+Vector2 getClCdWithYaw(float yawAngle, nlohmann::json* constFile);
+Vector2 getClCdWithPitch(float pitchAngle, nlohmann::json* constFile);
+Vector2 getClCdWithPitchAndYaw(float pitchAngle, float yawAngle, nlohmann::json* constFile);
+Vector2 getConstFromLiftFile(float pitchAngle, float yawAngle, nlohmann::json* constFile);
