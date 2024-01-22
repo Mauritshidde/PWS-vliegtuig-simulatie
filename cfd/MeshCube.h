2732e2f233a63ba9687f0273423bb15e26d1618a
Mauritshidde <Mauritshidde@gmail.com>
2024-01-22 15:22:13 +0100

    onnodige code en comments weg
    en navierstokes.h en .cpp terug geplaats in juiste folder

--- cfd/MeshCube.h
+++ cfd/MeshCube.h
@@ -3,17 +3,17 @@ class MeshCube
 private:
     /* data */
 public:
-    float velocityXDirection;
-    float velocityYDirection;
-    float velocityZDirection;
+    float velocityX; // velocity in x direction
+    float velocityY; // velocity in y direction
+    float velocityZ; // velocity in z direction
     float pressure;
 
     bool boundary;
 
     void makeBoundary() {boundary = true;};
-    void setVX(float vx) {velocityXDirection = vx;};
-    void setVY(float vy) {velocityYDirection = vy;};
-    void setVZ(float vz) {velocityZDirection = vz;};
+    void setVX(float vx) {velocityX = vx;};
+    void setVY(float vy) {velocityY = vy;};
+    void setVZ(float vz) {velocityZ = vz;};
     void setPressure(float newPressure) {pressure = newPressure;};
 
     void makeBoundary() {boundary = true;};
